38888e8cd1e7eb91b348b62ecbdb5da4d42ab01e
JanghooLee <dlwkdgn1@naver.com>
2020-05-20 17:49:32 +0900

    main function 주석 수정
    주석 수정

--- VS/Test-main.cpp
+++ VS/Test-main.cpp
@@ -10,13 +10,14 @@ int main()
 	while (game_master.get_turnAvailable())
 	{
 		game_master.turnCycleStart();
+		// 일부 플레이어가 멸망해도 _player_cnt 는 초깃값에서 변하지 않습니다.
 		for (int i = 0; i < game_master.get_playerCnt(); i++)
 		{
 			Player player = game_master.get_player(i);
 			int action_available = game_master.playerTrunStart(player);
 			for (int j = action_available; j > 0; j--)
 			{
-				// 각 플레이어의 행동 명령은 해당 객체에서 선택지 보여주고 제어
+				// 플레이어 클래스 : game_master.playerActionStart() 에서 실행.
 				game_master.playerActionStart(player);
 				game_master.playerActionEnd(player);
 			}
