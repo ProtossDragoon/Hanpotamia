e69bbc35c804201fee508df44846f5909e4d8d66
임동재 <djlim1998@gmail.com>
2020-05-21 18:10:10 +0900

    자원 병력 클래스 작성

--- VS/resource.h
+++ VS/resource.h
@@ -1,9 +1,24 @@
 #pragma once
 class Resource
 {
+	//물 금 식량
+private:
+	int Resource_Gold;
+	int Resource_Water;
+	int Resource_Food;
 
+protected:
+	//xx
 
+public:
+	//getter
+	int getGold();
+	int getFood();
+	int getWater();
 
-
+	//setter
+	void setGold();
+	void setFood();
+	void setWater();
 
 };
