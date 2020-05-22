6e97360a0f4c45d0cccd6c877fffe014d2e88efe
kim tae heon <palgalow@gmail.com>
2020-05-22 23:11:45 +0900

    map클래스 업데이트

--- VS/map.h
+++ VS/map.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <iostream>
+#include"player.h"
+#include"resource.h"
 using namespace std;
 
 typedef struct {
 	string areaname;
 	int areanum;
+	string areatype;
+	int arealevel;
+	string neighborarea[30];
 	string areahost;
 }areainformation;
 
@@ -12,9 +17,9 @@ class Map
 {
 private:
 	char _area;
-	player* _host_player;
-	resource _occupation_acquirable_resource;
-	resource _occupation_cost;
+	Player* _host_player;
+	Resource _occupation_acquirable_resource;
+	Resource _occupation_cost;
 	int _area_level;
 	int _area_number;
 	string _area_name;
@@ -22,23 +27,25 @@ private:
 	int _acquirable_resource_water;
 	int _acquirable_resource_gold;
 	int _acquirable_resource_food;
-	const int _max_area;
+	const int _max_area = 30;
 	int** _route;
-	areainformation area[25] = { NULL };
+	areainformation area[30] = { NULL };
 
 public:
 	Map(int _max_area);
-	void get_occupationPlayer();
+	string get_occupationPlayer() { return _host_player->get_play_name; }
 	void get_occupationCost();
-	void get_acquirableResource();
+	void get_acquirableResource(areainformation area);
 	void get_upgradeCost();
-	void get_movableArea(int start);
+	int get_movableArea(areainformation area[], int start);
 
 	void set_areaName();
 	void set_occupationCost();
-	void set_areaLevel();
-	void set_areaInformation();
-	void set_areaType();
+	void set_areaInformation(areainformation area[]);
+	void set_areaHost(Player* _host_player);
+	int set_acquirableFood(areainformation area);
+	int set_acquirableGold(areainformation area);
+	int set_acquirableWater(areainformation area);
 
 	int calBattle();
 };
