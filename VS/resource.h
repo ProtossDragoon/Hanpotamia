87a3543af8a25bb4222b63db3c3b270957850467
JanghooLee <dlwkdgn1@naver.com>
2020-05-24 15:11:36 +0900

    엥이거왜안돼있지?

--- VS/resource.h
+++ VS/resource.h
@@ -3,22 +3,26 @@ class Resource
 {
 	//물 금 식량
 private:
-	int Resource_Gold;
-	int Resource_Water;
-	int Resource_Food;
+	int _resource_gold;
+	int _resource_water;
+	int _resource_food;
 
 protected:
 	//xx
 
 public:
-	//getter
-	int getGold();
-	int getFood();
-	int getWater();
+	////constructor
+	//Resource();
+	//~Resource();
 
-	//setter
-	void setGold();
-	void setFood();
-	void setWater();
+	//getter
+	int get_resource_gold() { return _resource_gold; };
+	int get_resource_food() { return _resource_food; };
+	int get_resource_water() { return _resource_water; };
+
+	//setter 사용안할듯
+	void set_resource_gold(int gold);
+	void set_resource_food(int food);
+	void set_resource_water(int water);
 
 };
