f578037f4cfafa4e64367194763742de70a03352
임동재 <djlim1998@gmail.com>
2020-05-28 18:57:15 +0900

    resource unit .h .cpp

--- VS/C-resource.cpp
+++ VS/C-resource.cpp
@@ -1,4 +1,5 @@
 #include "resource.h"
+#include "string.h"
 
 ////construtor
 //Resource::Resource() {
@@ -16,4 +17,36 @@ void Resource::set_resource_food(int food)
 void Resource::set_resource_water(int water)
 {
 	_resource_water = water;
+}
+
+//void Resource::set_resource_gold(int areatype, int gold)
+//{
+//	if (areatype == 0)//areatype==0 -> ground
+//	{
+//		//1.  _resource_gold += 100;
+//		//2.  _resource_gold += gold;
+//	}
+//	else if (areatype == 1)//areatype==1 -> water
+//	{
+//		//1.  _resource_gold += 10;
+//		//2.  _resource_gold += gold;
+//	}
+//	cout << " " << endl;
+//	/*else if (areatype == 2)areatype==2 -> ground&&water
+//	{
+//
+//	}*/
+//}
+
+Resource Resource::calculate_produce_unit(string tendency, int cnt)
+{
+	Resource troop;
+	if (tendency=="Navy") {
+		troop.set_resource_food(get_);
+	}
+}
+
+void Resource::calculate_upgrade_area() 
+{
+
 }
