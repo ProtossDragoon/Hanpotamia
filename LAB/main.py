8c4fcaefdc2ee66218bee1e63a8e83163d7c8b42
JanghooLee <dlwkdgn1@naver.com>
2020-05-29 10:32:47 +0900

    Pygame 대폭 추가
    sprite 활용, 우측 정보 표시, 플레이어 표시, 토글 애니메이션 등 추가

--- LAB/main.py
+++ LAB/main.py
@@ -3,8 +3,8 @@ import random
 
 from __init__ import *
 
-from surfaces import Console, BackgroundMap, Charactor, Minimap, Dashboard, Explaination, City
-from sprites import BlockSprite
+from surfaces import Console, BackgroundMap, Charactor, Minimap, Explaination, City
+from sprites import Dashboard, DashboardPlayerImage, DashboardPlayerName
 
 from pygame.locals import *
 import pygame
@@ -12,41 +12,60 @@ import pygame
 
 if __name__ == '__main__' :
     
-    players = ['me', 'you']
+    players = ['me', 'you', 'fuckyou', 'hello']
     
     pygame.init()
     pygame.font.init()
     clock = pygame.time.Clock()
-    
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
     pygame.display.set_caption('Hanpotamia')
     programIcon = pygame.image.load('imgsrc/worrior_color/worrior_red.png')
     pygame.display.set_icon(programIcon)
     
+    # 찐텐 배경 관련
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
+        
+    # 우측 약간 검은부분 관련
     console = Console(screen)
+    
+    # 배경 및 city 렌더링 관련
     background_surface = BackgroundMap(screen, console.consolesize[0])
 
-    charactor_block_list = []
-    if len(players) >= 2:
-        # worrior1 = Charactor(screen, )
-        # worrior2 = Charactor(screen, 'imgsrc/worrior_color/worrior_blue.png')
-        worrior1 = BlockSprite((50,50), 'imgsrc/worrior_color/worrior_red.png')
-        worrior2 = BlockSprite((50,100), 'imgsrc/worrior_color/worrior_blue.png')
-        charactor_block_list.append(worrior1)
-        charactor_block_list.append(worrior2)        
-    if len(players) >= 3:
-        # worrior3 = Charactor(screen, 'imgsrc/worrior_color/worrior_green.png')
-        worrior3 = BlockSprite((50,50), 'imgsrc/worrior_color/worrior_green.png')
-        charactor_block_list.append(worrior3)
-    if len(players) >= 4:
-        # worrior4 = Charactor(screen, 'imgsrc/worrior_color/worrior_pink.png')
-        worrior4 = BlockSprite((50,50), 'imgsrc/worrior_color/worrior_pink.png')
-        charactor_block_list.append(worrior4)
-    
-    charactor_block_group = pygame.sprite.RenderPlain(*blocks)
- 
-    
+    # 설명창 관련
     areaexplain = Explaination(screen)
+
+    # 대시보드 관련
+    charactor_block_list = []
+    username_block_list = []
+    consolemargin = 0
+    dashboardSetting = Dashboard(1080+30, 30, 100)
+    if len(players) >= 2:
+        worrior1 = DashboardPlayerImage((dashboardSetting.charx,50), 'imgsrc/worrior_color/worrior_red.png')
+        worrior2 = DashboardPlayerImage((dashboardSetting.charx,100), 'imgsrc/worrior_color/worrior_blue.png')
+        player1name = DashboardPlayerName(players[0], (dashboardSetting.namex, 50), WHITE)
+        player2name = DashboardPlayerName(players[1], (dashboardSetting.namex, 100), WHITE)
+        charactor_block_list.append(worrior1)
+        charactor_block_list.append(worrior2)
+        username_block_list.append(player1name)
+        username_block_list.append(player2name)
+        consolemargin += 100        
+    if len(players) >= 3:
+        worrior3 = DashboardPlayerImage((dashboardSetting.charx,150), 'imgsrc/worrior_color/worrior_green.png')
+        player3name = DashboardPlayerName(players[2], (dashboardSetting.namex, 150), WHITE)
+        charactor_block_list.append(worrior3)
+        username_block_list.append(player3name)
+        consolemargin += 50
+    if len(players) >= 4:
+        worrior4 = DashboardPlayerImage((dashboardSetting.charx,200), 'imgsrc/worrior_color/worrior_pink.png')
+        player4name = DashboardPlayerName(players[3], (dashboardSetting.namex, 200), WHITE)
+        charactor_block_list.append(worrior4)
+        username_block_list.append(player4name)
+        consolemargin += 50
+    consolemargin += 10
+
+    console.set_textprinting_topmargin_for_dashboard(consolemargin)
+    charactor_block_group = pygame.sprite.RenderPlain(*charactor_block_list)
+    username_block_group = pygame.sprite.RenderPlain(*username_block_list)    
+
     
     run = True
     i = 0
@@ -57,7 +76,7 @@ if __name__ == '__main__' :
         #                user area                     #
         ################################################
         
-        clock.tick(20) # Max FPS
+        clock.tick(10) # Max FPS
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -65,15 +84,15 @@ if __name__ == '__main__' :
 
         
         console.insertData(['data', i]) # insert data and update console screen
-#        worrior1.update((50,50))
-#        worrior2.update((50,100))
-
+        charactor_block_group.draw(screen)
+        username_block_group.draw(screen)
 
         flag = 0
         for areaname in background_surface.city :
             print(areaname)
             if background_surface.city[areaname].rect.collidepoint(pygame.mouse.get_pos()):
                 areaexplain.showDiscription([areaname + '-gu','','hello~ lololol'])
+                background_surface.city[areaname].temporaryAttention()
                 flag = 1
         if flag == 0 :
             print('don\'t show discription')
