8fcc73e4d57f30907bc42aea52d95ddb975a27eb
JanghooLee <dlwkdgn1@naver.com>
2020-06-10 02:56:41 +0900

    마스터클래스 유저수입력부 추가

--- VS/Test-main.cpp
+++ VS/Test-main.cpp
@@ -1,12 +1,16 @@
 #include"master.h"
 #include"player.h"
+#include<iostream>
 
 static Master game_master = Master();
 
 int main()
 {
+	int player_cnt;
+	std::cin >> player_cnt;
+
 	// select player
-	game_master.gameReady(3);
+	game_master.gameReady(player_cnt);
 
 	// 게임이 끝났는지 끝나지 않았는지 최대 턴을 보고 판단
 	while (game_master.get_turnAvailable())
